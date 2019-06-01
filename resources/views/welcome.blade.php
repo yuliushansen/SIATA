5473e96dc223f74957015828605b6bafade5129f
Yulius Hansen <yuliushansen8@gmail.com>
2019-06-01 20:03:55 +0700

    Change Name Website
    Change name from "Laravel" to "SIATA"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
     <script>window.Laravel = { crsfToken: '{{ csrf_token() }}' }</script>
-    <title>Laravel</title>
+    <title>Siata</title>
     <style>
     body {
   overflow-x: hidden;
